4b46107829526f1c61240b5c39c2f8cec2ff8d71
Cotter Smart <cas324@lehigh.edu>
2024-02-26 15:32:54 -0500

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,156 @@
-import * as React from "react"
+/* IMPORTS */
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+import * as React from 'react'
+import { Link } from 'gatsby'
+/* import { graphql, useStaticQuery } from 'gatsby'
+import { getImage, GatsbyImage } from 'gatsby-plugin-image'
+import styled from 'styled-components'
+//import './App.css'
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+import BackgroundImage from 'gatsby-background-image'
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+//import backgroundImage from '/Users/cottersmart/Desktop/my-first-gatsby-site/src/background.jpg'
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+//import { graphql, useStaticQuery } from 'gatsby';
+//import { getImage, GatsbyImage } from 'gatsby-plugin-image';
+//import styled from 'styled-components';
+//import './App.css';
+
+//import BackgroundImage from 'gatsby-background-image';
+
+// Assuming backgroundImage is correctly imported and available
+//import backgroundImage from '/Users/cottersmart/Desktop/my-first-gatsby-site/src/background.jpg';
+
+*/
+/* CODE */
+
+
 
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
+  return (
+    <main>
+    
+     
+<h1 style={{ color: '#272727', fontFamily: 'Playfair Display', textAlign: 'center', fontSize: '55px', fontWeight: 'bold'}}>ESummarAIze</h1>
+
+      <Link to="/about">About</Link>
+
+      <select style={{fontFamily: 'Playfair Display'}} className='form-select'>
+        {options.map(option => (
+            <option value = {option.value}>{option.label}</option>
         ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
+      </select>
+
+      <div style={squareStyle}></div>
+
+      <button style={buttonRun}>Run</button>
+
+      <h2 style={{ color: 'black', fontFamily: 'Playfair Display', marginLeft: '330px', fontSize: '30px', fontWeight: 'bold'}}>Summary</h2>
+
+      <div style={squareStyle}></div>
+
+      <button style={buttonDownload}>Download</button>
+      <button style={buttonPrint}>Print</button>
+      <button style={buttonCopy}>Copy</button>
+
+      <p>MADE BY JULIA, COTTER, MATT!!</p>
+    </main>
+  
+
+)
+
 }
+
+
+
+export const Head = () => <title>Home Page</title>
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+
+const options = [
+    {label: "Select Date", value: 1},
+    {label: "From Last Meeting", value: 2},
+    {label: "Last Month", value: 2},
+    {label: "Last 3 Months", value: 3},
+    {label: "Last Year", value: 2},
+];
+
+
+const buttonRun = {
+      width: '63px',
+      height: '27px',
+      backgroundColor: '#bddaec',
+      border: '1px solid #85acc5',
+      borderRadius: '10px',
+      fontSize: '16px',
+      color: 'black',
+      fontFamily: 'Playfair Display',
+      cursor: 'pointer',
+      position: 'absolute',
+      bottom: '299px',
+      right: '340px', 
+    };
+
+    const buttonDownload = {
+          width: '82px',
+          height: '27px',
+          backgroundColor: '#bddaec',
+          border: '1px solid #85acc5',
+          borderRadius: '10px',
+          fontSize: '16px',
+          color: 'black',
+          fontFamily: 'Playfair Display',
+          cursor: 'pointer',
+          position: 'absolute',
+          bottom: '-190px',
+          right: '480px',
+        };
+
+        const buttonCopy = {
+              width: '63px',
+              height: '27px',
+              backgroundColor: '#bddaec',
+              border: '1px solid #85acc5',
+              borderRadius: '10px',
+              fontSize: '16px',
+              color: 'black',
+              fontFamily: 'Playfair Display',
+              cursor: 'pointer',
+              position: 'absolute',
+              bottom: '-190px',
+              right: '340px',
+            };
+
+            const buttonPrint = {
+                  width: '63px',
+                  height: '27px',
+                  backgroundColor: '#bddaec',
+                  border: '1px solid #85acc5',
+                  borderRadius: '10px',
+                  fontSize: '16px',
+                  color: 'black',
+                  fontFamily: 'Playfair Display',
+                  cursor: 'pointer',
+                  position: 'absolute',
+                  bottom: '-190px',
+                  right: '410px',
+                };
+
+
+    const squareStyle = {
+          width: '1000px',
+          height: '400px',
+          border: '2px solid black',
+          margin: '20px auto', // Centers the square horizontally
+          position: 'relative'
+        };
+
+
+
+
+
+        
